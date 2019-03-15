c85dd618bf65d7e695b995ee5ed19958efa4cbde
jarrettallman <46567429+jarrettallman@users.noreply.github.com>
2019-03-15 13:40:01 -0400

    Add files via upload
    NetTest powershell script

---
+++ NetTest.ps1
@@ -0,0 +1,24 @@
+﻿######Simple multi-host network connectivity test
+######Author: Jarrett Allman
+######Date: 3/15/2019
+
+
+###Specify CSV source for network list in format name,ip
+$netlist = Import-CSV nettest.csv
+
+###Get Date for file name
+$DateFileName = Get-Date -format "dd-MM-yyyy@HH-mm-ss"
+
+###Write site name and run test-netconnection for each entry in CSV
+$output =
+
+    ForEach($network in $netlist)
+        {
+        $name = $network.name
+        $ip = $network.ip
+        Write-Output ==============$name==============;Test-NetConnection -ComputerName $ip -InformationLevel Detailed
+        }
+
+
+##Output results to txt file
+$output | Out-File "NetTest_$DateFileName.txt"
